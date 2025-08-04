66efa8d74be5a26b2dd4751d8881e74cb98ae5ef
Lorenzo Rutigliano <lnz.rutigliano@gmail.com>
2025-08-04 14:55:55 +0200

    changes

--- arch-post.sh
+++ arch-post.sh
@@ -3,7 +3,7 @@ set -e
 
 ####
 FULL=true
-DE="qtile"
+DE="hyprland"
 ####
 
 ## Commands
@@ -101,22 +101,39 @@ function xserver() {
 }
 
 
-function common() {
-  core
-  aur_helper
-  terminal
-  fonts
+function install_sddm() {
+  # Install SDDM display manager
+  # Check sddm them at https://framagit.org/MarianArlt/sddm-sugar-candy
+  $AUR qt6-base qt6-declarative qt5-base qt5-declarative qt5-graphicaleffects qt5-quickcontrols2 qt5-svg sddm sddm-sugar-dark
+  sudo systemctl enable sddm.service
   $SL
 }
 
+function install_display_manager() {
+  local DM=$1
 
-# function install_sddm() {
-#   # Install SDDM display manager
-#   # Check sddm them at https://framagit.org/MarianArlt/sddm-sugar-candy
-#   $AUR qt6-base qt6-declarative qt5-base qt5-declarative qt5-graphicaleffects qt5-quickcontrols2 qt5-svg sddm sddm-sugar-dark
-#   sudo systemctl enable sddm
-#   $SL
-# }
+  case $DM in
+    "sddm")
+      echo -n "Installing SDDM ..."
+      # Check sddm them at https://framagit.org/MarianArlt/sddm-sugar-candy
+      $AUR qt6-base qt6-declarative qt5-base qt5-declarative \
+        qt5-graphicaleffects qt5-quickcontrols2 qt5-svg \
+        sddm sddm-sugar-dark
+      sudo systemctl enable sddm.service
+      ;;
+    "ly")
+      echo -n "Installing ly ..."
+      $AUR ly
+      sudo systemctl enable ly.service
+      ;;
+    *)
+      echo -n "Invalid variable name."
+      exit 1
+      ;;
+  esac 
+  
+  $SL
+}
 
 
 function install_gnome() {
@@ -159,23 +176,41 @@ function install_hyprpanel() {
 
 function install_hyprland() {
   # Common
-  $AUR qt5-wayland qt6-wayland
+  $AUR wayland gtk4 qt5-base qt5-declarative qt5-wayland qt6-base qt6-declarative qt6-wayland
 
   # UWSM
   $AUR uwsm libnewt
+
+  # Install Hyprland Libraries
+  $AUR \
+    hyprutils \
+    hyprlang \
+    hyprcursor \
+    hyprwayland-scanner \
+    aquamarine \
+    hyprgraphics \
+    hyprland-qt-support \
+    hyprland-qtutils
+
+  # Install Hyprland Utils
+  $AUR \
+    hyprpaper \
+    hyprpicker \
+    hypridle \
+    hyprlock \
+    hyprsunset \
+    xdg-desktop-portal-hyprland \
+    hyprsysteminfo \
+    hyprpolkitagent
 
   # hyprland
   $AUR hyprland
 
   # display manager
-  install_display_manager
+  install_display_manager "ly"
 
   # hyprpanel
-  install_hyprpanel
-
-  $AUR \
-    hyprpaper \
-    xdg-desktop-portal-hyprland
+  # install_hyprpanel
 
   $SL
 }
@@ -205,31 +240,37 @@ function dotfiles() {
 
 function main() {
   init
-  common
+  core
+  aur_helper
+  fonts
+  terminal
+  $SL
 
-  if $FULL; then
-    case $DE in
-      "gnome")
-        echo -n "Installing GNOME desktop environment."
-        install_gnome
-        extra
-        dotfiles
-        ;;
-      "hyprland")
-        echo -n "Installing QTILE desktop environment."
-        install_hyprland
-        extra
-        dotfiles
-        ;;
-      *)
-        echo -n "Invalid variiable name."
-        exit 1
-        ;;
-    esac
+  # if $FULL; then
+  #   case $DE in
+  #     "gnome")
+  #       echo -n "Installing GNOME desktop environment."
+  #       install_gnome
+  #       extra
+  #       dotfiles
+  #       ;;
+  #     "hyprland")
+  #       echo -n "Installing QTILE desktop environment."
+  #       install_hyprland
+  #       extra
+  #       dotfiles
+  #       ;;
+  #     *)
+  #       echo -n "Invalid variiable name."
+  #       exit 1
+  #       ;;
+  #   esac
 
-    extra
-    dotfiles
-  fi
+  #   dotfiles
+  # fi
+
+  install_hyprland
+
   $SL
 }
 
