cb38c0e2c20cd308c3563f8ec3250f6d3c742ec5
Lorenzo Rutigliano <lnz.rutigliano@gmail.com>
2025-11-26 17:01:54 +0100

    c

--- arch-install-hyprland.sh
+++ arch-install-hyprland.sh
@@ -22,34 +22,19 @@ init () {
 
 core() {
   sudo pacman -Syyuq --noconfirm
-  # Configure Runtimes/DevTools Env Variables
-  sudo pacman -S --noconfirm --needed mise
-  mise set -g PYTHON_HISTORY="$XDG_DATA_HOME/python/history"
-  mise set -g PYTHONSTARTUP="$XDG_CONFIG_HOME/python/pythonrc"
-  mise set -g NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME/npm/npmrc"
-  mise set -g GOPATH="$XDG_DATA_HOME/go"
-  mise set -g GOBIN="$GOPATH/bin"
-  mise set -g GOMODCACHE="$XDG_CACHE_HOME/go/mod"
-  mise set -g CARGO_HOME="$XDG_DATA_HOME/cargo"
-  mise set -g RUSTUP_HOME="$XDG_DATA_HOME/rustup"
-  mise set -g TF_DATA_DIR="$XDG_DATA_HOME/terraform"
-  mise set -g TF_PLUGIN_CACHE_DIR="$XDG_CACHE_HOME/terraform/plugins"
-  mise set -g TERRAGRUNT_DOWNLOAD="$XDG_CACHE_HOME/terragrunt"
-  mise set -g ANSIBLE_HOME="$XDG_DATA_HOME/ansible"
-  mise set -g HELM_CACHE_HOME="$XDG_CACHE_HOME/helm"
-  mise set -g HELM_CONFIG_HOME="$XDG_CONFIG_HOME/helm"
-  mise set -g HELM_DATA_HOME="$XDG_DATA_HOME/helm"
-  mise set -g HELMFILE_CACHE_HOME="$XDG_CACHE_HOME/helmfile"
-  mise use -g python@latest
-  mise use -g node@latest
-  mise use -g go@latest
-  mise use -g rust@latest
-  mise use -g terraform@latest opentofu@latest terragrunt@latest
-  mise use -g ansible@latest
-  mise use -g helm@latest
-  mise use -g helmfile@latest
-  mise activate
+}
 
+aur_helper() {
+  local aur_home="/home/$USER/paru"
+  if ! command -v paru >/dev/null; then
+    [ ! -d $aur_home ] && git -C /home/$USER clone https://aur.archlinux.org/paru-bin.git
+    (cd $aur_home && makepkg -mcsi --needed --noconfirm --noprogressbar)
+    sudo sed -i 's/#BottomUp/BottomUp/' /etc/paru.conf
+    rm -Rf ${aur_home}
+  fi
+}
+
+docker() {
   if ! command -v docker >/dev/null; then
 	  # Install Docker
 	  sudo pacman -S --noconfirm docker
@@ -64,23 +49,13 @@ core() {
   fi
 }
 
-aur_helper() {
-  local aur_home="/home/$USER/paru"
-  if ! command -v paru >/dev/null; then
-    [ ! -d $aur_home ] && git -C /home/$USER clone https://aur.archlinux.org/paru.git
-    (cd $aur_home && makepkg -mcsi --needed --noconfirm --noprogressbar)
-    sudo sed -i 's/#BottomUp/BottomUp/' /etc/paru.conf
-    rm -Rf ${aur_home}
-  fi
-}
-
 wayland() {
   $AUR      \
     wayland \
-    gtk4    \
+    gtk3 gtk4 \
     qt5-base qt5-declarative qt5-wayland \
     qt6-base qt6-declarative qt6-wayland \
-    xdg-desktop-portal xdg-desktop-portal-hyprland \
+    xdg-desktop-portal \
     uwsm libnewt \
     xorg-xwayland
 }
@@ -88,8 +63,8 @@ wayland() {
 fonts() {
   $AUR \
     noto-fonts \
-	noto-fonts-cjk \
-	noto-fonts-emoji \
+	  noto-fonts-cjk \
+	  noto-fonts-emoji \
     ttf-dejavu \
     ttf-ubuntu-mono-nerd \
     ttf-hack \
@@ -100,7 +75,7 @@ fonts() {
 
 terminal() {
   $AUR        \
-    kitty     \
+    ghostty   \
     foot      \
     zsh       \
     starship  \
@@ -121,12 +96,7 @@ display_manager() {
     "sddm")
       echo -n "Installing SDDM ..."
       # Check sddm them at https://framagit.org/MarianArlt/sddm-sugar-candy
-      $AUR \
-        qt5-graphicaleffects \
-        qt5-quickcontrols2 \
-        qt5-svg \
-        sddm \
-        sddm-sugar-dark
+      $AUR sddm
       sudo systemctl enable sddm.service
       ;;
     "ly")
@@ -143,21 +113,21 @@ display_manager() {
 
 utils() {
   # File utilities
-  $AUR bat eza jq yq fd ripgrep fzf zoxide resvg imagemagick wl-clipboard yazi
+  $AUR bat eza fd ripgrep fzf zoxide resvg imagemagick wl-clipboard yazi jq yq
   # Audio
-  $AUR pipewire pipewire-pulse pipewire-alsa pipewire-jack wireplumber pavucontrol pulsemixer
+  $AUR pipewire pipewire-pulse pipewire-alsa pipewire-jack wireplumber
   # Multimedia
   $AUR ffmpeg gstreamer gst-libav gst-plugin-pipewire vlc
-  # Notifications & panels
-  $AUR libnotify gvfs swaync waybar
+  # Notifications
+  $AUR libnotify gvfs swaync
   # Theming + wallpapers
   $AUR matugen-bin swww
   # Launcher
-  $AUR wofi
+  $AUR rofi-wayland
   # Editors & LSPs
   # $AUR neovim bash-language-server lua-language-server pyright gopls rust-analyzer
   # PDF
-  $AUR sqlite file zathura zathura-pdf-mupdf
+  # $AUR sqlite file zathura zathura-pdf-mupdf
 }
 
 install_hyprland() {
@@ -216,6 +186,19 @@ dotfiles() {
   local config="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
   $config checkout
   $config config --local status.showUntrackedFiles no
+  source $HOME/.zshenv
+}
+
+mise() {
+  # Configure Runtimes/DevTools Env Variables
+  $AUR mise
+  mise use -g uv@latest pipx@latest python@latest
+  mise use -g node@latest
+  mise use -g go@latest
+  mise use -g rust@latest
+  mise use -g terraform@latest opentofu@latest terragrunt@latest
+  mise use -g ansible@latest
+  mise use -g helm@latest helmfile@latest
 }
 
 
@@ -223,6 +206,7 @@ function main() {
   init
   core
   aur_helper
+  docker
   wayland
   fonts
   terminal
@@ -232,6 +216,7 @@ function main() {
   display_manager
   extra
   dotfiles
+  mise
 }
 
 time main "$@"
